489fa8c245d625b227e18f218bd1145a52d92df6
Michael <michaelwithnospaces@proton.me>
2024-11-06 00:08:17 -0600

    The great reset :(

--- Card.cpp
+++ Card.cpp
@@ -3,43 +3,26 @@
 #include "Card.h"
 using std::string;
 
-Card::Card(string rank, string suit) : rank(rank), suit(suit) 
-{
-    // constructor checks
-    for (char c : rank)
-    {
-        if (!isalnum(c)) throw std::invalid_argument("Rank contains a value that is not alphanumeric.");
-    }
-    for (char c : suit)
-    {
-        if (!isalnum(c)) throw std::invalid_argument("Suit contains a value that is not alphanumeric.");
-    }
+Card::Card(string rank, string suit) /* TODO: initialize */ {
+    // TODO: implement constructor checks
 }
 
-string Card::getRank()
-{
-    return this->rank;
+string Card::getRank(){
+    // TODO: implement getter
 }
 
-string Card::getSuit()
-{
-    return this->suit;
+string Card::getSuit(){
+    // TODO: implement getter
 }
 
-int Card::getTimesPlayed()
-{
-    return this->timesPlayed;
+int Card::getTimesPlayed(){
+    // TODO: implement getter
 }
 
-bool Card::canBePlayed(string currentRank, string currentSuit)
-{
-    // return whether or not the card can legally be played
-    if (this->rank == "8") return true;
-    if (currentRank == this->rank || currentSuit == this->suit) return true;
-    return false; 
+bool Card::canBePlayed(string currentRank, string currentSuit){
+    // TODO: return whether or not the card can legally be played
 }
 
-void Card::play()
-{
+void Card::play(){
     timesPlayed++;
 }
--- main.cpp
+++ main.cpp
@@ -6,33 +6,26 @@
 #include "Game.h"
 using std::string;
 
-bool loadDeck(Game& g)
-{
+bool loadDeck(Game& g){
     string filename;
     std::cout << "Choose a file to load the deck from:" << std::endl;
     std::cin >> filename;
-    try
-    {
+    try{
         g.loadDeckFromFile(filename);
     }
-    catch(std::runtime_error const&)
-    {
+    catch(std::runtime_error const&){
         std::cout << "The file was invalid. Aborting." << std::endl;
         return false;
     }
     return true;
 }
 
-int getPlayerCount()
-{
+int getPlayerCount(){
     std::cout << "Enter number of players:" << std::endl;
     int numPlayers;
-    while(true)
-    {
-        if((!(std::cin >> numPlayers)) || numPlayers <= 0)
-        {
-            if(std::cin.fail())
-            {
+    while(true){
+        if((!(std::cin >> numPlayers)) || numPlayers <= 0){
+            if(std::cin.fail()){
                 //Clear the fail flag
                 std::cin.clear();
                 //Remove the non-integer data from the stream
@@ -41,8 +34,7 @@ int getPlayerCount()
             }
             std::cout << "Please enter a positive number" << std::endl;
         }
-        else
-        {
+        else{
             //break out of the loop once we've read a valid number
             break;
         }
@@ -50,39 +42,29 @@ int getPlayerCount()
     return numPlayers;
 }
 
-void setupPlayers(Game& g, int numPlayers)
-{
+void setupPlayers(Game& g, int numPlayers){
     // TODO: Determine whether each player is a human or an AI
     // and add them to the game
-    for (int i=0 ; i<numPlayers ; i++)
-    {
-        
-    }
 }
 
-void setupGame(Game& g)
-{
+void setupGame(Game& g){
     // TODO: Determine how many cards to deal, deal the cards, and
     // print the initial discard
 }
 
-int main()
-{
+int main(){
     Game g;
-    if(!loadDeck(g))
-    {
+    if(!loadDeck(g)){
         return 1;
     }
     int numPlayers = getPlayerCount();
     setupPlayers(g,numPlayers);
     setupGame(g);
     int winner = g.runGame();
-    if(winner != -1)
-    {
+    if(winner != -1){
         std::cout << "Player " << winner << " wins!" << std::endl;
     }
-    else
-    {
+    else{
         std::cout << "The game is a draw!" << std::endl;
     }
     std::cout << "The most played suit was " << g.mostPlayedSuit() << std::endl;
